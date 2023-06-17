eee95d04e01985ea15df5f2e7e887ef246a33e6e
BCygal <b.cygal@interia.pl>
2023-06-17 11:52:05 +0200

    add serviceworker

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,3 +79,13 @@ let searchPosition = (position) => {
     weatherForm.requestSubmit()
 
 }
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
